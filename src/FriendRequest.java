397e5897ca135f2353662b052acd658f757006dc
siaaarora <75089968+siaaarora@users.noreply.github.com>
2024-06-01 12:29:47 -0700

    Update FriendRequest.java

--- src/FriendRequest.java
+++ src/FriendRequest.java
@@ -1,10 +1,5 @@
 /**
  * The requests are used to view sent and received friend requests.
- *
- * <p>Team 20-1 Project 5 -- CS18000 -- Fall 2020</p>
- *
- * @author Group 20-1
- * @version December 6, 2020
  */
 
 public class FriendRequest {
