9c56d55bf565169d6b5cc3763d5c20f9860de29c
siaaarora <75089968+siaaarora@users.noreply.github.com>
2024-06-01 12:29:21 -0700

    Update Homepage.java

--- src/Homepage.java
+++ src/Homepage.java
@@ -13,11 +13,6 @@ import javax.swing.plaf.FontUIResource;
 
 /**
  * The user descriptions can be changed using Edit profile.
- *
- * <p>Team 20-1 Project 5 -- CS18000 -- Fall 2020</p>
- *
- * @author Group 20-1
- * @version December 6, 2020
  */
 
 public class Homepage extends JFrame implements ActionListener {
